42c26dc83922e35abf586b5434ac87bfbd59fb8b
Flavio Moreira <flaviorsm@gmail.com>
2018-06-30 21:38:10 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,11 @@
 export const environment = {
     production: false,
     firebase : {
-<<<<<<< HEAD
-        apiKey: "AIzaSyCDxaG0dknIfaoWTmO7Iy7WzBCZoFuPK8s",
-        authDomain: "fisioterapia-aplicada.firebaseapp.com",
-        databaseURL: "https://fisioterapia-aplicada.firebaseio.com",
-        projectId: "fisioterapia-aplicada",
-        storageBucket: "fisioterapia-aplicada.appspot.com",
-        messagingSenderId: "858806391323",
-        timestampsInSnapshots: true
-=======
       apiKey: '### FIREBASE API KEY ###',
       authDomain: '### FIREBASE AUTH DOMAIN ###',
       projectId: '### CLOUD FIRESTORE PROJECT ID ###',                    
       databaseURL: "###",
       storageBucket: "####",
       messagingSenderId: "######"
->>>>>>> c1eb4a5745657af560ffe8dae34d4d73f61682ae
     }
  };
